55dc51da047f2700d97943b7d2f5242ff61acb33
Marcos Junior <marcosjuunioor2@gmail.com>
2021-02-11 10:04:54 -0300

    Criação das primeiras classes de teste

---
+++ src/test/java/descorp/agendamentoweb/tests/ClienteTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package descorp.agendamentoweb.tests;
+
+import descorp.agendamentoweb.entities.Cliente;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marco
+ */
+public class ClienteTest extends GenericTest{
+   
+    @Test
+    public void persistirCliente(){
+        Cliente cliente = criarCliente();
+        em.persist(cliente);
+        em.flush();
+        assertNotNull(cliente.getId());
+        System.out.print(cliente.getId());
+    }
+    
+    @Test
+    public void consultarCliente(){
+        Cliente cliente = em.find(Cliente.class, 1L);
+        assertEquals("jose@gmail.com", cliente.getEmail());
+        assertEquals("81988771456", cliente.getTelefone());
+        assertEquals("1234", cliente.getSenha());
+        assertEquals("José", cliente.getNome());
+        assertEquals("12345678910", cliente.getCPF());
+        
+    }
+    
+        
+    
+    private static Cliente criarCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setEmail("jose@gmail.com");
+        cliente.setSenha("1234");
+        cliente.setTelefone("81988771456");
+        cliente.setNome("José");
+        cliente.setCPF("12345678910");
+        return cliente;
+    }
+    
+}
---
+++ src/test/java/descorp/agendamentoweb/tests/GenericTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package descorp.agendamentoweb.tests;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author marco
+ */
+public class GenericTest {
+    protected static EntityManagerFactory emf;
+    protected EntityManager em;
+    protected EntityTransaction et;
+    
+    @BeforeClass
+    public static void setUpClass() {
+        emf = Persistence.createEntityManagerFactory("agendamento_web");
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        emf.close();
+    }
+
+    @Before
+    public void setUp() {
+        em = emf.createEntityManager();
+        beginTransaction();
+    }
+
+    @After
+    public void tearDown() {
+        commitTransaction();
+        em.close();      
+    }
+
+    protected void beginTransaction() {
+        et = em.getTransaction();
+        et.begin();
+    }
+
+    protected void commitTransaction() {
+        try {
+            et.commit();
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+    }
+}
