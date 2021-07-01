334122ab2fa3f772e67a1de28c09d8ef79424616
BeaCristina <biacristinafs@hotmail.com>
2021-06-30 23:47:58 -0300

    Trello: criar rotina para lista os procedimentos com os quais o estabelecimento trabalha

--- src/main/java/descorp/agendamentoweb/servlets/ProcedimentoServlet.java
+++ src/main/java/descorp/agendamentoweb/servlets/ProcedimentoServlet.java
@@ -5,8 +5,11 @@
  */
 package descorp.agendamentoweb.servlets;
 
+import descorp.agendamentoweb.entities.Procedimento;
+import descorp.agendamentoweb.entities.Profissional;
+import descorp.agendamentoweb.models.ProcedimentoModel;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,68 +20,66 @@ import javax.servlet.http.HttpServletResponse;
  * @author marco
  */
 public class ProcedimentoServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProcedimentoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProcedimentoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    
+    private final ProcedimentoModel procedimentoModel;
+    
+    public ProcedimentoServlet(){
+        this.procedimentoModel = new ProcedimentoModel();
     }
+    
+    // path = procedimentos/procedimento (GET)
+    protected void getProcedimento(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        Long idUsuario = 2L;
+        request.getSession().setAttribute("idUsuario", idUsuario);
+        request.getRequestDispatcher("/procedimentos.xhtml").forward(request, response);
+    }
+    //path = procedimentos/listar-procedimentos
+    protected void getListaProcedimentos(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");  
+        String json = "[";
+        
+        for(Procedimento proc: this.procedimentoModel.todosProcedimentos()){
+            String procedimento = "{'id':"+proc.getId()+",'nome':'"+proc.getNome()+"','natureza':'"+proc.getNatureza()+"','duracao':'"+fmt.format(proc.getDuracao())+"','profissionais':[";
+            for(Profissional profissional: proc.getProfissionais()){
+                procedimento += "{'id':"+profissional.getId()+", 'nome':'"+profissional.getNome()+"'},";
+            }
+            procedimento += "]},";
+            
+            json += procedimento;
+        }
+        json += "]";
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+        
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //processRequest(request, response);
-        request.getRequestDispatcher("/procedimentos.xhtml").forward(request, response);
+        String endPoint = request.getRequestURI().split("/")[3];
+        System.out.println(endPoint);
+        switch (endPoint) {
+            case "procedimento":
+                this.getProcedimento(request, response);
+                break;
+            case "listar-procedimentos":
+                this.getListaProcedimentos(request, response);
+                break;
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
