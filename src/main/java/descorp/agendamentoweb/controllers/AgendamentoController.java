54f8e4bb36167f6a6510d923c6d051d3f62bcb71
Thiago <taoalu@gmail.com>
2021-07-07 01:31:57 -0300

    Criação da persistência do agendamento.

--- src/main/java/descorp/agendamentoweb/controllers/AgendamentoController.java
+++ src/main/java/descorp/agendamentoweb/controllers/AgendamentoController.java
@@ -28,28 +28,35 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class AgendamentoController implements Serializable {
 
+    List<Agendamento> agendamentosDia;
+
     @EJB
     private final AgendamentoModel bean;
-    private final ArrayList<String> horariosDisponiveis;
+    private final ArrayList<String> horariosEstabelecimento;
+    private final ArrayList<Agendamento> horariosDisponiveis;
 
     public AgendamentoController() {
         this.bean = new AgendamentoModel();
-        this.horariosDisponiveis = new ArrayList<String>();
-
+        this.horariosEstabelecimento = new ArrayList<String>();
+        this.horariosDisponiveis = new ArrayList<Agendamento>();
     }
 
-    public List<String> getHorariosIndisponiveis(String dataSelecionada, String idProf, String idProc) {
+    public List<Agendamento> getHorariosIndisponiveis(String dataSelecionada, String idProf, String idProc) {
+
+        AgendamentoModel agendamentoModel = new AgendamentoModel();
+
         if (this.horariosDisponiveis == null || this.horariosDisponiveis.isEmpty()) {
+
             Long idProcedimento = Long.valueOf(idProc);
             Long idProfissional = Long.valueOf(idProf);
 
-            //Horários nos quais o estabelecimento faz agendamentos
-            this.horariosDisponiveis.add("08:00");
-            this.horariosDisponiveis.add("10:00");
-            this.horariosDisponiveis.add("12:00");
-            this.horariosDisponiveis.add("14:00");
-            this.horariosDisponiveis.add("16:00");
-            this.horariosDisponiveis.add("18:00");
+            //Horários nos quais o estabelecimento faz agendamentos            
+            this.horariosEstabelecimento.add("08:00");
+            this.horariosEstabelecimento.add("10:00");
+            this.horariosEstabelecimento.add("12:00");
+            this.horariosEstabelecimento.add("14:00");
+            this.horariosEstabelecimento.add("16:00");
+            this.horariosEstabelecimento.add("18:00");
 
             try {
                 String startDateString = dataSelecionada;
@@ -59,12 +66,16 @@ public class AgendamentoController implements Serializable {
 
                 Date dataSaida = sdf2.parse(saida);
 
-                List<Agendamento> agendamentos = this.bean.consultarHorariosIndisponiveis(idProfissional, idProcedimento, dataSaida);
+                agendamentosDia = this.bean.consultarHorariosIndisponiveis(idProfissional, idProcedimento, dataSaida);
 
-                for (Agendamento a : agendamentos) {
-                    String hora = getDuracaoFMT(a.getHora());
-                    if (this.horariosDisponiveis.contains(hora)) {
-                        this.horariosDisponiveis.remove(hora);
+                if (agendamentosDia.size() > 0) {
+                    for (int i = 0; i < horariosEstabelecimento.size(); i++) {
+                        for (int x = 0; x < agendamentosDia.size(); x++) {
+                            String hora = getDuracaoFMT(agendamentosDia.get(x).getHora());
+                            if (!this.horariosEstabelecimento.get(i).equals(hora)) {
+                                this.horariosDisponiveis.add(agendamentoModel.criarAgendamento(dataSaida, agendamentoModel.criarHora(Integer.parseInt(this.horariosEstabelecimento.get(i).substring(0, 2)), 0, 0), idProcedimento, idProfissional));
+                            }
+                        }
                     }
                 }
 
@@ -83,4 +94,9 @@ public class AgendamentoController implements Serializable {
         String duracaoSTR = fmt.format(duracao);
         return duracaoSTR;
     }
+
+    public void criarAgendamento(Agendamento agendamento) {
+        bean.persistirAgendamento(agendamento);
+    }
+
 }
